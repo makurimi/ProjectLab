d5aa4550c0819f1e902f6fa5f5528b7c17813f2e
nathertan <ndihyan@gmail.com>
2022-12-23 13:23:17 +0700

    migration add, fix imminent

--- database/migrations/2022_12_23_061601_create_product_infos_table.php
+++ database/migrations/2022_12_23_061601_create_product_infos_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('product_infos', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('image');
+            $table->string('name');
+            $table->string('detail');
+            $table->string('price');
+            $table->bigInteger('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('categories');
         });
     }
 
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('product_infos');
     }
 };
